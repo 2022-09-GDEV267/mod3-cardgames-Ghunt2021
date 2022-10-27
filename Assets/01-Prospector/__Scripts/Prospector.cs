f1dfc94498e4d5f4ba6c15ab687c5169203c14a8
Ghunt2021 <89885988+Ghunt2021@users.noreply.github.com>
2022-10-27 00:07:19 -0400

    Replace Card With CardProspectord

--- Assets/01-Prospector/__Scripts/Prospector.cs
+++ Assets/01-Prospector/__Scripts/Prospector.cs
@@ -19,6 +19,8 @@ public class Prospector : MonoBehaviour {
 
 	public Layout layout;
 
+	public List<CardProspector> drawPile;
+
 	void Awake(){
 		S = this;
 	}
@@ -45,6 +47,27 @@ public class Prospector : MonoBehaviour {
 		layout = GetComponent<Layout>();  // Get the Layout component
 
 		layout.ReadLayout(layoutXML.text); // Pass LayoutXML to it
+
+		drawPile = ConvertListCardsToListCardProspectors(deck.cards);
 	}
 
+	List<CardProspector> ConvertListCardsToListCardProspectors(List<Card> lCD)
+	{
+
+		List<CardProspector> lCP = new List<CardProspector>();
+
+		CardProspector tCP;
+
+		foreach (Card tCD in lCD)
+		{
+
+			tCP = tCD as CardProspector;                                     // a
+
+			lCP.Add(tCP);
+
+		}
+
+		return (lCP);
+
+	}
 }
