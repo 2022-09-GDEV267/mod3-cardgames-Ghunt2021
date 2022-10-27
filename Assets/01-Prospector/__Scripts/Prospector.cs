103f9300992db61f146d7eacd79c094410867c60
Ghunt2021 <89885988+Ghunt2021@users.noreply.github.com>
2022-10-26 23:17:47 -0400

    s01 - First part of Prospector
    Implementing Prospector in Code

--- Assets/01-Prospector/__Scripts/Prospector.cs
+++ Assets/01-Prospector/__Scripts/Prospector.cs
@@ -10,11 +10,14 @@ public class Prospector : MonoBehaviour {
 	static public Prospector 	S;
 
 	[Header("Set in Inspector")]
-	public TextAsset			deckXML;
+	public TextAsset deckXML;
 
+	public TextAsset layoutXML;
 
 	[Header("Set Dynamically")]
-	public Deck					deck;
+	public Deck	deck;
+
+	public Layout layout;
 
 	void Awake(){
 		S = this;
@@ -23,6 +26,25 @@ public class Prospector : MonoBehaviour {
 	void Start() {
 		deck = GetComponent<Deck> ();
 		deck.InitDeck (deckXML.text);
+
+		Deck.Shuffle(ref deck.cards); // This shuffles the deck by reference // a
+
+
+		//Comment out this section
+		//Card c;
+		//
+		//for (int cNum = 0; cNum < deck.cards.Count; cNum++)
+		//{                    // b
+		//
+		//	c = deck.cards[cNum];
+		//
+		//	c.transform.localPosition = new Vector3((cNum % 13) * 3, cNum / 13 * 4, 0);
+		//
+		//}
+
+		layout = GetComponent<Layout>();  // Get the Layout component
+
+		layout.ReadLayout(layoutXML.text); // Pass LayoutXML to it
 	}
 
 }
