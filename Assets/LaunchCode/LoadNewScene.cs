d5ef1905d4f31493e0e52684c4f4fc9f225b6a7a
Ghunt2021 <89885988+Ghunt2021@users.noreply.github.com>
2022-11-03 13:59:53 -0400

    BUTTONS WORK

---
+++ Assets/LaunchCode/LoadNewScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNewScene : MonoBehaviour
+{
+    public void LoadProspector()
+    {
+        SceneManager.LoadScene("__Prospector");
+    }
+
+    public void LoadPyramid()
+    {
+        SceneManager.LoadScene("_Pyramid");
+    }
+}
